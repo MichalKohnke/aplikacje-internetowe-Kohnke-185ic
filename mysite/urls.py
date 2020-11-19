fc76137e989b49ea9082c9b5b7d35fb10f2daccd
MichalKohnke <185ickohnkem@gmail.com>
2020-11-19 23:11:31 +0100

    Test lab2

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+	url('accounts/', include('accounts.urls')),
 	url('accounts/', include('django.contrib.auth.urls')),
     url(r'', include('blog.urls')),
 ]
